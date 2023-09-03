f7ef1d633e18273807e65c9850445c02c6482087
Nika Farqosadze <nfarqosadze12365@gmail.com>
2023-09-03 21:22:04 +0400

    win logic added

--- tictactoe.py
+++ tictactoe.py
@@ -17,11 +17,19 @@ markers = []
 clicked = False
 pos = []
 player = 1
-
+winner = 0
+game_over = False
 
 # colors
 green = (0, 255, 0)
 red = (255, 0, 0)
+blue = (0, 0, 255)
+
+# font
+font = pygame.font.SysFont(None, 40)
+
+# play again rect
+again_rect = Rect(screen_width // 2 - 80, screen_height // 2, 160, 50)
 
 # Gameboard
 
@@ -57,7 +65,48 @@ def draw_markers():
                     screen, red, (x_pos * 100 + 50, y_pos * 100+50), 38, line_width)
             y_pos += 1
         x_pos += 1
-        
+
+
+def check_winner():
+    global winner, game_over
+    y_pos = 0
+    for x in markers:
+        # check cols
+        if sum(x) == 3:
+            winner = 1
+            game_over = True
+        if sum(x) == -3:
+            winner = 2
+            game_over = True
+        # check rows
+        if markers[0][y_pos] + markers[1][y_pos] + markers[2][y_pos] == 3:
+            winner = 1
+            game_over = True
+        if markers[0][y_pos] + markers[1][y_pos] + markers[2][y_pos] == -3:
+            winner = 2
+            game_over = True
+        y_pos += 1
+
+        # check diagonally
+        if markers[0][0] + markers[1][1] + markers[2][2] == 3 or markers[2][0] + markers[1][1] + markers[0][2] == 3:
+            winner = 1
+            game_over = True
+        if markers[0][0] + markers[1][1] + markers[2][2] == 3 or markers[2][0] + markers[1][1] + markers[0][2] == -3:
+            winner = 2
+            game_over = True
+
+
+def draw_winner(winner):
+    win_text = 'Player ' + str(winner) + ' wins!'
+    win_img = font.render(win_text, True, blue)
+    pygame.draw.rect(screen, green, (screen_width // 2 -
+                     100, screen_height // 2 - 60, 200, 50))
+    screen.blit(win_img, (screen_width // 2 - 100, screen_height // 2 - 50))
+
+    again_text = 'Play Again?'
+    again_img = font.render(again_text, True, blue)
+    pygame.draw.rect(screen, green, again_rect)
+    screen.blit(again_img, (screen_width // 2 - 80, screen_height // 2 + 10))
 
 
 while True:
@@ -65,18 +114,39 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        if game_over == 0:
+            if event.type == pygame.MOUSEBUTTONDOWN and clicked == False:
+                clicked = True
+            if event.type == pygame.MOUSEBUTTONUP and clicked == True:
+                clicked = False
+                pos = pygame.mouse.get_pos()
+                cell_x = pos[0]
+                cell_y = pos[1]
+                if markers[cell_x // 100][cell_y//100] == 0:
+                    markers[cell_x // 100][cell_y//100] = player
+                    player *= -1
+                    check_winner()
+
+    draw_grid()
+    draw_markers()
+
+    if game_over == True:
+        draw_winner(winner)
+        # check play again
+        if event.type == pygame.MOUSEBUTTONDOWN and clicked == False:
             clicked = True
         if event.type == pygame.MOUSEBUTTONUP and clicked == True:
             clicked = False
             pos = pygame.mouse.get_pos()
-            cell_x = pos[0]
-            cell_y = pos[1]
-            if markers[cell_x // 100][cell_y//100] == 0:
-                markers[cell_x // 100][cell_y//100] = player
-                player *= -1
-
-    draw_grid()
-    draw_markers()
+            if again_rect.collidepoint(pos):
+                # reset all
+                markers = []
+                pos = []
+                player = 1
+                winner = 0
+                game_over = False
+                for x in range(3):
+                    row = [0] * 3
+                    markers.append(row)
 
     pygame.display.update()
